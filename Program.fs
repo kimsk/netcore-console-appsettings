2b559e0eb2ea8ef79cd1505336607a7cf2f3fb65
Karlkim Suwamongkol <contact@karlk.im>
2019-05-07 19:54:19 -0500

    netcore

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Microsoft.Extensions.Configuration
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let builder = 
+      ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+
+    let configuration = builder.Build()
+
+    let setting = configuration.GetSection("test").["test"]
+    printfn "%s" setting
+    0
