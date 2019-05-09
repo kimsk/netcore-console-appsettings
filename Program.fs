260c8395a6788388f47f9dd31835678e5faa8cae
Karlkim Suwanmongkol <karlkim@tachyus.com>
2019-05-09 13:36:59 -0500

    test DI

--- Program.fs
+++ Program.fs
@@ -1,8 +1,7 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open Microsoft.Extensions.Configuration
+﻿open System
 open System.IO
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
 
 [<EntryPoint>]
 let main argv =
@@ -10,9 +9,24 @@ let main argv =
       ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", true, true)
+        .AddEnvironmentVariables()
 
     let configuration = builder.Build()
 
-    let setting = configuration.GetSection("test").["test"]
+    let serviceCollection =
+      ServiceCollection()
+        .AddSingleton<IConfigurationRoot>(configuration)
+        .BuildServiceProvider()
+      
+    let c = serviceCollection.GetService<IConfigurationRoot>()
+
+    let setting = c.GetSection("test").["test"]
+    let environment = c.GetSection("ASPNETCORE_ENVIRONMENT").Value
     printfn "%s" setting
+    printfn "%s" environment
+
+    // get environment variable using System.Environment
+    Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+    |> printfn "%s"
+
     0
